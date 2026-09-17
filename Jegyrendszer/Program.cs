f243026166945ef2c43be5fcb159322150cb616f
kristofav6584 <kovacsk3@kkszki.hu>
2026-09-17 11:37:15 +0200

    napi bevétel

--- Jegyrendszer/Program.cs
+++ Jegyrendszer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jegyrendszer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,21 @@ namespace Jegyrendszer
     {
         static void Main(string[] args)
         {
+            List<Utazas> napiUtazasok = new List<Utazas>
+            {
+                new Vonalbusz(50, 10),
+                new MeteoVonat(450, 25),
+                new ElektromosRoller(5.5),
+                new Vonalbusz(450, 3)
+            };
+
+            int bevetel = 0;
+
+            foreach (Utazas utazas in napiUtazasok)
+            {
+                bevetel += utazas.ArSzamitas();
+            }
+            Console.WriteLine($"A napi bevétel: {bevetel}Ft");
         }
     }
 }
