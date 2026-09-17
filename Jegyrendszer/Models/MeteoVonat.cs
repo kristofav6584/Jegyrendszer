2cae4afe899fb015d48150c1150bb871dc7ef01d
kristofav6584 <kovacsk3@kkszki.hu>
2026-09-17 11:18:10 +0200

    MeteoVonat osztály és árszámítás

--- Jegyrendszer/Models/MeteoVonat.cs
+++ Jegyrendszer/Models/MeteoVonat.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Jegyrendszer.Models
 {
-    internal class MeteoVonat
+    internal class MeteoVonat : Utazas
     {
+        public MeteoVonat(int alapdij, double tavolsag) : base(alapdij, tavolsag){ }
+
+        public override int ArSzamitas()
+        {
+            return (Alapdij * 2) + (int)(Tavolsag * 50);
+        }
     }
 }
