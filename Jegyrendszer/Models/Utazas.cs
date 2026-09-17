7e1fbdade3b0fcd4d276ea9eba4d63af7dc8e20f
kristofav6584 <kovacsk3@kkszki.hu>
2026-09-17 10:47:25 +0200

    Osztályok

---
+++ Jegyrendszer/Models/Utazas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jegyrendszer.Models
+{
+    internal class Utazas
+    {
+    }
+}
