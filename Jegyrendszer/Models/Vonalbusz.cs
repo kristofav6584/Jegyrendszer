59ef3add58d2aeb5ad3de5a63a4dbec29bca9128
kristofav6584 <kovacsk3@kkszki.hu>
2026-09-17 11:13:11 +0200

    Vonalbusz osztály és árszámítás

--- Jegyrendszer/Models/Vonalbusz.cs
+++ Jegyrendszer/Models/Vonalbusz.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Jegyrendszer.Models
 {
-    internal class Vonalbusz
+    public class Vonalbusz : Utazas
     {
+        public Vonalbusz(int alapdij, double tavolsag) : base(alapdij, tavolsag)
+        { }
+            public override int ArSzamitas()
+        { return Alapdij + (int)(Tavolsag * 30); }
     }
 }
+
